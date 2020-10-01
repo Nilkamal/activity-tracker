728aff3eda1661011b2e1461e3639a9434a465c6
Nilkamal Shah <nilkamalsha75@gmail.com>
2020-10-01 16:56:16 +0530

    Updates

---
+++ precache-manifest.618564e447b6aa5782cba382631e5207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834b6f1bbe72c6de4609e5a956eb55c1",
+    "url": "/activity-tracker/index.html"
+  },
+  {
+    "revision": "993aa9809d85c34bf068",
+    "url": "/activity-tracker/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6f7c3a92195bc67aed00",
+    "url": "/activity-tracker/static/css/main.125d1477.chunk.css"
+  },
+  {
+    "revision": "993aa9809d85c34bf068",
+    "url": "/activity-tracker/static/js/2.c343726f.chunk.js"
+  },
+  {
+    "revision": "930ea72c1567d102c6a6ce440bc27014",
+    "url": "/activity-tracker/static/js/2.c343726f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7c3a92195bc67aed00",
+    "url": "/activity-tracker/static/js/main.fc36fda4.chunk.js"
+  },
+  {
+    "revision": "66dcc5d305bb3922622e",
+    "url": "/activity-tracker/static/js/runtime-main.ef064ab4.js"
+  }
+]);
